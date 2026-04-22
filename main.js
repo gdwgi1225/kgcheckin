6393f33721879a605ff309650080e7c594f1144b
gdwgi1225 <gdwgi1225@gmail.com>
2026-04-22 19:27:33 +0800

    today

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+import { execSync } from "child_process";
 import { printBlue, printGreen, printMagenta, printRed, printYellow } from "./utils/colorOut.js";
 import { close_api, delay, send, startService } from "./utils/utils.js";
 
 async function main() {
 
   const USERINFO = process.env.USERINFO
+  const PAT = process.env.PAT
+  // 刷新token
+  const refreshUserinfo = []
+  let needRefresh = false
   if (!USERINFO) {
     throw new Error("未配置")
   }
@@ -38,6 +43,19 @@ async function main() {
         continue
       }
       printMagenta(`账号 ${userDetail?.data?.nickname} 开始领取VIP...`)
+
+      // 周日刷新token
+      if (today.getDay() == 0) {
+        const refreshToken = await send(`/login/token?timestrap=${Date.now()}`, "POST", headers)
+        if (refreshToken?.status == 1) {
+          if (refreshToken?.data?.token !== user.token) {
+            needRefresh = true
+            printYellow(`账号 ${userDetail?.data?.nickname} 需要刷新token`)
+            user.token = refreshToken.data.token
+          }
+        }
+        refreshUserinfo.push(user)
+      }
 
       // 开始听歌
       printYellow(`开始听歌领取VIP...`)
@@ -84,9 +102,30 @@ async function main() {
         errorMsg[userDetail?.data?.nickname + " vip_details"] = vip_details
       }
     }
+
   } finally {
     close_api(api)
   }
+
+  // 更新secret <USERINFO>
+  if (refreshUserinfo.length > 0 && needRefresh) {
+
+    if (PAT) {
+      const userinfoJSON = JSON.stringify(refreshUserinfo)
+      try {
+        // printGreen(userinfoJSON)
+        execSync(`gh secret set USERINFO -b'${userinfoJSON}' --repo ${process.env.GITHUB_REPOSITORY}`);
+        printGreen("secret <USERINFO> token刷新成功")
+      } catch (error) {
+        printRed("token刷新失败")
+        throw error
+      }
+    } else {
+      printYellow("存在账号需要刷新token，但是未配置PAT，未刷新token最多两个月后过期")
+    }
+
+  }
+
   if (Object.keys(errorMsg).length > 0) {
     printRed("异常信息如下:")
     console.dir(errorMsg, { depth: null })
@@ -99,4 +138,3 @@ async function main() {
 }
 
 main()
-
